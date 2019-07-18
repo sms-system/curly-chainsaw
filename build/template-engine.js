41550a385066c40a99b65ceb62542aa1e25b9c87
Serge Smirnov <sms@cert.kz>
2019-07-19 02:34:53 +0600

    Fix template engine for autotests

--- build/template-engine.js
+++ build/template-engine.js
@@ -1,7 +1,8 @@
 // WARNING!! Not for production use !!
 // Has not XSS prevention, not optimized
 
-const config = require('../config.json')
+import config from '../config.json'
+
 const ELEM_SEPARATOR = config.BEM_SEPARATORS.ELEM
 const MOD_SEPARATOR = config.BEM_SEPARATORS.MOD
 
