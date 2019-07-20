f5581fb89373f700ab3c8e664bf191e9bc2abcf1
Serge Smirnov <sms@cert.kz>
2019-07-21 01:24:54 +0600

    Added theme switching

---
+++ build/script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Theme switch logic
+
+  function switchTheme () {
+    const themeAlternates = {
+      'theme_color_project-inverse': 'theme_color_project-default',
+      'theme_color_project-default': 'theme_color_project-inverse'
+    }
+    let themedBlocks = {}
+    Object.keys(themeAlternates).forEach((className) => {
+      themedBlocks[className] = Array.from(document.getElementsByClassName(className))
+    })
+    Object.keys(themeAlternates).forEach((className) => {
+      themedBlocks[className].forEach((block) => {
+        block.classList.remove(className)
+        block.classList.add(themeAlternates[className])
+      })
+    })
+  }
+
+  const onoffswitch = document.getElementsByClassName('onoffswitch')[0]
+  const onoffswitchCheckedClassName = 'onoffswitch_checked'
+  let onoffswitchIsChecked = false
+  if (Array.from(onoffswitch.classList).includes(onoffswitchCheckedClassName)) {
+    onoffswitchIsChecked = true
+  }
+  onoffswitch.addEventListener('click', () => {
+    onoffswitchIsChecked = !onoffswitchIsChecked
+    if (onoffswitchIsChecked) { onoffswitch.classList.add(onoffswitchCheckedClassName) }
+    else { onoffswitch.classList.remove(onoffswitchCheckedClassName) }
+    switchTheme()
+  })
+
+})
